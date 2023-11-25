bf1785f7506dc179c41ea2825ff5489985a3f42b
Guillaume <contact@gfauconnet.fr>
2023-11-25 23:49:24 +0100

    Revert commit

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :altamessengerapi, Altamessengerapi.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "altamessengerapi_dev",
+  database: "alta_messenger_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
