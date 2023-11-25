b390c1376131f208d4bd2c1e8806047f70555566
Guillaume <contact@gfauconnet.fr>
2023-11-26 00:05:07 +0100

    Recreate User Context/Schema/Migration with uuid

---
+++ priv/repo/migrations/20231125230400_create_users.exs
@@ -0,0 +1,12 @@
+defmodule Altamessengerapi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :username, :string
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
